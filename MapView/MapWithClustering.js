02e734d21791d347828e64e1a839b76619145337
Alex Sinner <sancheskinghtc@gmail.com>
2018-08-06 21:04:06 +0300

    add check for notCluster prop

--- MapView/MapWithClustering.js
+++ MapView/MapWithClustering.js
@@ -50,7 +50,7 @@ export default class MapWithClustering extends Component {
     const otherChildren = [];
 
     React.Children.forEach(this.props.children, (marker) => {
-      if (marker && marker.props && marker.props.coordinate) {
+      if (marker && marker.props && marker.props.coordinate && !marker.props.notCluster) {
         markers.push({
           marker,
           properties: { point_count: 0 },
